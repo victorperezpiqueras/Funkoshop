3a1482ea0a4012e647d10a7577da2428ce740674
carmenmoreno <carmen98mi@gmail.com>
2019-10-31 20:27:55 +0100

    Casi acabado signup

--- public/funkoshop/views/controller/signup.js
+++ public/funkoshop/views/controller/signup.js
@@ -4,3 +4,41 @@ Controller.controllers.signup.refresh = function (matching) {
     View.renderer.signup.render(context);
 
 }
+
+Controller.controllers.signup.signup_clicked = function (event, bid) {
+    event.preventDefault();
+    var userInfo = {
+        name: $('#name').val(),
+        surname: $('#surname').val(),
+        address: $('#address').val(),
+        birth: $('#birth').val(),
+        email: $('#email').val(),
+        password: $('#password').val(),
+        confirmpassword: $('#confirmpassword').val()
+    }
+
+    //var ok = !userInfo.name.length && !userInfo.surname.length && !userInfo.address.length && !userInfo.birth.length && !userInfo.email.length && !userInfo.password.length && !userInfo.confirmpassword.length;
+    var ok = !userInfo.name.length || !userInfo.surname.length || !userInfo.address.length || !userInfo.birth.length || !userInfo.email.length || !userInfo.password.length || !userInfo.confirmpassword.length;
+    var equalpasswd = false;
+    if (userInfo.password == userInfo.confirmpassword){
+        equalpasswd = true;
+        console.log('TRUE');
+    }
+    if (!ok && equalpasswd) {
+        Model.signup(userInfo)
+            .then(function () {
+                console.log('Comment added successfully');
+            })
+            .catch(function (err) {
+                console.error('Comment cannot be added', err);
+            })
+            .then(function () {
+                Controller.router.go('/funkoshop/views/index');
+            });
+    } else {
+        console.error('MAL');
+
+        this.refresh;
+    }
+
+}

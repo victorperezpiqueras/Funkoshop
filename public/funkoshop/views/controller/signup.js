7033c5c09edafbb58e0c5f3bbf7799c2c8b1a118
carmenmoreno <carmen98mi@gmail.com>
2019-11-06 22:50:36 +0100

    Casi terminando el signup

--- public/funkoshop/views/controller/signup.js
+++ public/funkoshop/views/controller/signup.js
@@ -1,22 +1,39 @@
 Controller.controllers.signup = {};
 Controller.controllers.signup.refresh = function () {
     var context = {};
-    View.renderer.signup.render(context);
+    //load badge and render
+    Model.loadBadge()
+        .then((counter) => {
+            context.counter = counter;
+        })
+        .then(() => {
+            View.renderer.signup.render(context);
+        });
     
 }
 
 Controller.controllers.signup.signup_clicked = function (event, bid) {
     event.preventDefault();
+    var date = $('#birth').val();
+    date = new Date(date);
     var userInfo = {
+        id : Date.now(),
         name: $('#name').val(),
         surname: $('#surname').val(),
         address: $('#address').val(),
-        birth: $('#birth').val(),
+        birth: date,
         email: $('#email').val(),
         password: $('#password').val(),
-        confirmpassword: $('#confirmpassword').val()
+        confirmpassword: $('#confirmpassword').val(),
+        userOrders: [],
+        shoppingCart: {
+            subtotal: 0,
+            tax:0.21,
+            total:0,
+            shoppingCartItems: []
+        }
     }
-
+    //birth = new Date(birth);
     var ok = !userInfo.name.length || !userInfo.surname.length || !userInfo.address.length || !userInfo.birth.length || !userInfo.email.length || !userInfo.password.length || !userInfo.confirmpassword.length;
     var equalpasswd = false;
     if (userInfo.password == userInfo.confirmpassword && !ok) {
@@ -36,13 +53,3 @@ Controller.controllers.signup.signup_clicked = function (event, bid) {
     }
 
     }
-
-
-    //load badge and render
-    Model.loadBadge()
-        .then((counter) => {
-            context.counter = counter;
-        })
-        .then(() => {
-            View.renderer.signup.render(context);
-        });
